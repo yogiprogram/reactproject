64c0742cf527804613d0045c494c618c586fcc98
Yogesh Wadile <ywa@marketlogicsoftware.com>
2018-10-21 00:44:44 +0530

    Authentication code

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require("express");
-const passportConfig = require("./services/passport");
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+require("./services/passport");
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+//connect to mongodb
+mongoose.connect(keys.mongodbURI);
 
+//cookie for user authentication
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//pass application paramter to authRoutes
+require("./routes/authRoutes")(app);
+
+//server port heoruko
 const PORT = process.env.PORT || 5001;
-
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,19 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -12,7 +25,19 @@ passport.use(
     (accessToken, refreshToken, profile, done) => {
       console.log("access token", accessToken);
       console.log("refresh token", refreshToken);
-      console.log("profile:", profile);
+      console.log("------- Saving user to mongodb start ---------", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //do nothing
+          done(null, existingUser);
+        } else {
+          //if not exsit save ionto db
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+      console.log("------- Saving user to mongodb end ---------");
     }
   )
 );
