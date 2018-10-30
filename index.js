2ebaab5636d6beffe09a94ae75b8abc80df3f16c
Yogesh Wadile <ywa@marketlogicsoftware.com>
2018-10-30 10:11:14 +0530

    Webhook for localtunnel

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ mongoose.connect(keys.mongoURI);
 app.use(bodyParser.json());
 //cookie for user authentication
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -33,15 +33,15 @@ require("./routes/billingRoutes")(app);
 require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
-   // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 //server port heoruko
 const PORT = process.env.PORT || 5001;
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const {URL} = require('url');
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -6,31 +9,63 @@ const surveyTemplate = require("../emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
-    consel.log('Thanks ');
-    res.send("Thanks for voting!");
-  });
-
-  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
-    const { title, subject, body, recipients } = req.body;
-    const survey = new Survey({
-      title,
-      subject,
-      body,
-      recipients: recipients.split(",").map(email => ({ email })),
-      _user: req.user.id,
-      dateSend: Date.now()
+    app.get("/api/surveys/:surveyId/:choice", (req, res) => {
+        console.log('Thanks ');
+        res.send("Thanks for voting!");
     });
-    // Create Mail
-    const mailer = new Mailer(survey, surveyTemplate(survey));
-    try {
-      await mailer.send();
-      await survey.save();
-      req.user.credits -= 1;
-      const user = await req.user.save();
-      res.send(user);
-    } catch (err) {
-      res.status(422).send(err);
-    }
-  });
+
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = p.test(new URL(url).pathname);
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice };
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne(
+                    {
+                        _id: surveyId,
+                        recipients: {
+                            $elemMatch: { email: email, responded: false }
+                        }
+                    },
+                    {
+                        $inc: { [choice]: 1 },
+                        $set: { 'recipients.$.responded': true },
+                        lastResponded: new Date()
+                    }
+                ).exec();
+            })
+            .value();
+            console.log("===== Updated ====");
+        res.send({});
+    });
+
+    app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+        const {title, subject, body, recipients} = req.body;
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(",").map(email => ({email: email.trim()})),
+            _user: req.user.id,
+            dateSend: Date.now()
+        });
+        // Create Mail
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        try {
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
+    });
 };
