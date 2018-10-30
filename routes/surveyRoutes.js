f61ec765c451b5017fe223b37b0eaeb1f4db9dff
Yogesh Wadile <ywa@marketlogicsoftware.com>
2018-10-30 11:45:42 +0530

    Add endpoint get list of servey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,6 +9,15 @@ const surveyTemplate = require("../emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find({_user: req.user.id})
+            .select({
+                recipients: false
+            });
+        res.send(surveys);
+    });
+
     app.get("/api/surveys/:surveyId/:choice", (req, res) => {
         console.log('Thanks ');
         res.send("Thanks for voting!");
@@ -18,31 +27,31 @@ module.exports = app => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
         _.chain(req.body)
-            .map(({ email, url }) => {
+            .map(({email, url}) => {
                 const match = p.test(new URL(url).pathname);
                 if (match) {
-                    return { email, surveyId: match.surveyId, choice: match.choice };
+                    return {email, surveyId: match.surveyId, choice: match.choice};
                 }
             })
             .compact()
             .uniqBy('email', 'surveyId')
-            .each(({ surveyId, email, choice }) => {
+            .each(({surveyId, email, choice}) => {
                 Survey.updateOne(
                     {
                         _id: surveyId,
                         recipients: {
-                            $elemMatch: { email: email, responded: false }
+                            $elemMatch: {email: email, responded: false}
                         }
                     },
                     {
-                        $inc: { [choice]: 1 },
-                        $set: { 'recipients.$.responded': true },
+                        $inc: {[choice]: 1},
+                        $set: {'recipients.$.responded': true},
                         lastResponded: new Date()
                     }
                 ).exec();
             })
             .value();
-            console.log("===== Updated ====");
+        console.log("===== Updated ====");
         res.send({});
     });
 
